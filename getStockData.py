71d8b46b6601d3f0cf76a347f3dc078a44f1949d
vik-handa <66447532+vik-handa@users.noreply.github.com>
2021-03-17 16:18:01 -0700

    Update getStockData.py

--- getStockData.py
+++ getStockData.py
@@ -144,70 +144,116 @@ df['1 Month Change'] = round(df['1 Month Change'], 4)
 df['5 Month Change'] = round(df['5 Month Change'], 4)
 df['adjClose'] = round(df['adjClose'], 4)
 
-#create dataframe to compare following variables
-analysis1 = pd.DataFrame(columns = ['Adjusted CAGR', 'Adj Std Dev', 'Adj Max DD', 'Adj SSR', 'Unadjusted CAGR', 'Unadj Std Dev', 'Unadj Max DD', 'Unadj SSR'])
-rowCount2 = len(analysis1.index)
+#create dataframe to compare following variables for NONTIMER and TIMER
+analysis1 = pd.DataFrame(columns = ['date', 'Adjusted Close', '1 Month Change', 'Monthly Change Std', 'Unadjusted Equity', 'Unadjusted CAGR', 'Max Equity', 'Dropdown', 'DD Percentage'])
+analysis2 = pd.DataFrame(columns = ['date', 'Adjusted Close', '1 Month Change', 'Monthly Change Std', 'Adjusted Equity', 'Adjusted CAGR', 'Max Equity', 'Dropdown', 'DD Percentage'])
 
-#copies CAGR values from main dataframe
-analysis1["Adjusted CAGR"] = df['Adjusted CAGR']
-analysis1["Unadjusted CAGR"] = df['Unadjusted CAGR']
+#copies general stock info (closing prices, equity, dropdowns, and dropdown percentage) from original dataframe
+analysis1["date"] = df['date']
+analysis1["Adjusted Close"] = df['adjClose']
+analysis1['1 Month Change'] = df['1 Month Change']
+analysis1['Unadjusted Equity'] = df['Unadjusted Equity']
+analysis1['Max Equity'] = df['Max Unadjusted']
+analysis1['Dropdown'] = df['DD Unadjusted']
+analysis1['DD Percentage'] = df['DD Unadjusted Percentage']
 
-#variables and loop is to update the standard deviation
-#and the max drawdowns overtime
-std_calculator1 = pd.Series(dtype = 'float64')
-std_calculator2 = pd.Series(dtype = 'float64')
-maxDD1 = 0
-maxDD2 = 0
-for i in range(12, rowCount):
-    std_calculator1.at[i] = analysis1.iloc[i,0]
-    analysis1.at[i, 'Adj Std Dev'] = std_calculator1.std()
+#computes standard deviation of monthly change and CAGR over 25 year span
+analysis1.at[0, 'Monthly Change Std'] = analysis1['1 Month Change'].std()
+analysis1.at[0, 'Unadjusted CAGR'] = (analysis1['Unadjusted Equity'].iloc[-1] / analysis1['Unadjusted Equity'].iloc[0]) ** (1 / years) - 1
 
-    std_calculator2.at[i] = analysis1.iloc[i, 4]
-    analysis1.at[i, 'Unadj Std Dev'] = std_calculator2.std()
+#copies general info again just as we did above
+analysis2["date"] = df['date']
+analysis2["Adjusted Close"] = df['adjClose']
+analysis2['1 Month Change'] = df['Adjusted Equity']
+analysis2['1 Month Change'] = round(analysis2['1 Month Change'].pct_change(), 4)
+analysis2['Adjusted Equity'] = df['Adjusted Equity']
+analysis2['Max Equity'] = df['Max Adjusted']
+analysis2['Dropdown'] = df['DD Adjusted']
+analysis2['DD Percentage'] = df['DD Adjusted Percentage']
 
-    if df.at[i, 'DD Adjusted'] > maxDD1:
-        maxDD1 = df.at[i, 'DD Adjusted']
-    analysis1.at[i, 'Adj Max DD'] = maxDD1
+#computes standard deviation of monthly change and CAGR over 25 year span
+analysis2.at[0, 'Monthly Change Std'] = analysis2['1 Month Change'].std()
+analysis2.at[0, 'Adjusted CAGR'] = (analysis2['Adjusted Equity'].iloc[-1] / analysis2['Adjusted Equity'].iloc[0]) ** (1 / years) - 1
 
-    if df.at[i, 'DD Unadjusted'] > maxDD2:
-        maxDD2 = df.at[i, 'DD Unadjusted']
-    analysis1.at[i, 'Unadj Max DD'] = maxDD2
+#create dataframes to compare TIMER and NONTIMER results within 5 year intervals
+rolling_unadjusted = pd.DataFrame(columns = ['Start Date', 'End Date', 'Std Dev', 'CAGR', 'Max Percentage DD', 'SSR'])
+rolling_adjusted = pd.DataFrame(columns = ['Start Date', 'End Date', 'Std Dev', 'CAGR', 'Max Percentage DD', 'SSR', 'Helped?', 'Help Stats'])
 
-    analysis1.at[i, 'Adj SSR'] = analysis1.at[i, 'Adjusted CAGR'] / analysis1.at[i, 'Adj Std Dev']
-    analysis1.at[i, 'Unadj SSR'] = analysis1.at[i, 'Unadjusted CAGR'] / analysis1.at[i, 'Unadj Std Dev']
+#pandas series used to help calculate standard deviations and maximum drawdowns
+std_calculator = pd.Series(dtype = 'float64')
+maxDD_calculator = pd.Series(dtype = 'float64')
 
-#second analysis dataframe follows same steps
-#as the first, but on a 5 year basis
-analysis2 = pd.DataFrame(columns = ['Adjusted CAGR', 'Adj Std Dev', 'Adj Max DD', 'Adj SSR', 'Unadjusted CAGR', 'Unadj Std Dev', 'Unadj Max DD', 'Unadj SSR'])
-rowCount3 = len(analysis2.index)
+#fills out start date and end date columns for NONTIMER
+#cuts off the last 5 rows (Not enough data for 5 year intervals after August 2020)
+rolling_unadjusted['Start Date'] = df['date']
+rolling_unadjusted['End Date'] = df['date']
+rolling_unadjusted['End Date'] = rolling_unadjusted['End Date'].shift(periods = -5)
+rolling_unadjusted = rolling_unadjusted[:-5]
+interval_row_count = rowCount - 5
 
-std_calculator3 = pd.Series(dtype = 'float64')
-std_calculator4 = pd.Series(dtype = 'float64')
-maxDD3 = 0
-maxDD4 = 0
+#for each interval
+for i in range(0, interval_row_count):
 
-for i in range(61, rowCount):
-    analysis2.at[i, 'Adjusted CAGR'] = ((df.at[i, 'Adjusted Equity'] / df.at[i - 60, 'Adjusted Equity']) ** (1 / 5) - 1)
-    analysis2.at[i, 'Unadjusted CAGR'] = ((df.at[i, 'Adjusted Equity'] / df.at[i - 60, 'Unadjusted Equity']) ** (1 / 5) - 1)
+    #add 5 monthly change values into series and find std dev
+    #add 5 dropdown percentage values into series and find max
+    for j in range(0,5):
+        std_calculator.at[j] = analysis1.iloc[i + j, 2]
+        maxDD_calculator.at[j] = analysis1.iloc[i + j, 8]
+        maxDD_calculator.at[j] = analysis1.iloc[i + j, 8]
+    rolling_unadjusted.at[i, 'Std Dev'] = round(std_calculator.std(), 4)
+    rolling_unadjusted.at[i, 'Max Percentage DD'] = maxDD_calculator.max()
 
-    std_calculator3.at[i] = analysis1.iloc[i, 0]
-    analysis2.at[i, 'Adj Std Dev'] = std_calculator3.std()
+    #compute CAGR over 5 year span, rounded for clarity
+    rolling_unadjusted.at[i, 'CAGR'] = round((analysis1.iloc[i + 5, 4] / analysis1.iloc[i, 4]) ** 0.2 - 1, 4)
 
-    std_calculator4.at[i] = analysis1.iloc[i, 4]
-    analysis2.at[i, 'Unadj Std Dev'] = std_calculator4.std()
+#computes simplifies sharpe ratio for each interval
+rolling_unadjusted['SSR'] = rolling_unadjusted['CAGR'] / rolling_unadjusted['Std Dev']
 
-    if df.at[i, 'DD Adjusted'] > maxDD3:
-        maxDD3 = df.at[i, 'DD Adjusted']
-    analysis2.at[i, 'Adj Max DD'] = maxDD3
 
-    if df.at[i, 'DD Unadjusted'] > maxDD4:
-        maxDD4 = df.at[i, 'DD Unadjusted']
-    analysis2.at[i, 'Unadj Max DD'] = maxDD4
+#copying dstock info again for intervals with TIMER
+rolling_adjusted['Start Date'] = df['date']
+rolling_adjusted['End Date'] = df['date']
+rolling_adjusted['End Date'] = rolling_adjusted['End Date'].shift(periods = -5)
+rolling_adjusted = rolling_adjusted[:-5]
 
-    analysis2.at[i, 'Adj SSR'] = analysis2.at[i, 'Adjusted CAGR'] / analysis1.at[i, 'Adj Std Dev']
-    analysis2.at[i, 'Unadj SSR'] = analysis2.at[i, 'Unadjusted CAGR'] / analysis1.at[i, 'Unadj Std Dev']
+#keeps track of number of intervals the timer helped
+help_count = 0
+
+#for each interval
+for i in range(0, interval_row_count):
+
+    #compute standard deviations and max of drawdown percentages
+    for j in range(0,5):
+        std_calculator.at[j] = analysis2.iloc[i + j, 2]
+        maxDD_calculator.at[j] = analysis2.iloc[i + j, 8]
+        maxDD_calculator.at[j] = analysis2.iloc[i + j, 8]
+    rolling_adjusted.at[i, 'Std Dev'] = round(std_calculator.std(), 4)
+    rolling_adjusted.at[i, 'Max Percentage DD'] = maxDD_calculator.max()
+
+    #compute CAGRs for 5 year interval, rounded for clarity
+    rolling_adjusted.at[i, 'CAGR'] = round((analysis2.iloc[i + 5, 4] / analysis2.iloc[i, 4]) ** 0.2 - 1, 4)
+    if rolling_adjusted.at[i, 'Std Dev'] != 0:
+        rolling_adjusted.at[i, 'SSR'] = rolling_adjusted.at[i, 'CAGR'] / rolling_adjusted.at[i, 'Std Dev']
+
+    #fills out if the timer helps if timer either
+    # improves sharpe ratio OR
+    # improves CAGR and reduces max DD percentage
+    if rolling_adjusted.at[i, 'SSR'] > rolling_unadjusted.at[i, 'SSR']:
+        rolling_adjusted.at[i, 'Helped?'] = 'Yes'
+        help_count += 1
+    if rolling_adjusted.at[i, 'CAGR'] > rolling_unadjusted.at[i, 'CAGR'] and rolling_adjusted.at[i, 'Max Percentage DD'] < rolling_unadjusted.at[i, 'Max Percentage DD']:
+        rolling_adjusted.at[i, 'Helped?'] = 'Yes'
+        help_count += 1
+
+# displays number of intervals helped and proportion of total intervals that were helped
+rolling_adjusted.at[0, 'Help Stats'] = 'Number of Intervals Helped: ' + str(help_count)
+rolling_adjusted.at[1, 'Help Stats'] = 'Proportion of Intervals Helped: ' + str(round(help_count / interval_row_count, 4))
+print(rolling_adjusted)
+
 
 #write dataframes to csv files
 df.to_csv('new25.csv')
-analysis1.to_csv('Summary1yr.csv')
-analysis2.to_csv('Summary5yr.csv')
+analysis1.to_csv('UnadjustedSummary.csv')
+analysis2.to_csv('AdjustedSummary.csv')
+rolling_unadjusted.to_csv('interval_no_timer.csv')
+rolling_adjusted.to_csv('interval_timer.csv')
